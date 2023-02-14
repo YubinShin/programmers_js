fe259ad00ee66be3b681fbca9ccb4e66b4e2fc0f
YubinShin <68121478+YubinShin@users.noreply.github.com>
2023-02-14 17:04:05 +0900

    [level 0] Title: 배열 뒤집기, Time: 3.01 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -0,0 +1,9 @@
+function solution(num_list) {
+    var answer = [];
+    var j = num_list.length;
+    for (var i = 1; i <= j; i++) {
+        answer.push(num_list[j - i])
+    }
+    console.log(answer);
+    return answer;
+}
