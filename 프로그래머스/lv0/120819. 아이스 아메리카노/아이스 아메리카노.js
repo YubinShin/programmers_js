837ccdc872c4c2cdef153736f012eca38b872753
YubinShin <68121478+YubinShin@users.noreply.github.com>
2023-02-20 23:05:33 +0900

    [level 0] Title: 아이스 아메리카노, Time: 2.88 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -0,0 +1,8 @@
+function solution(money) {
+    var answer = [];
+    let 아아메 = 5500;
+    answer.push(Math.floor(money / 아아메));
+    answer.push(money % 아아메);
+    console.log(answer);
+    return answer;
+}
